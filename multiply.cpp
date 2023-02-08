0e62638dcb8204061d356f1906a5b56fcccb13e8
Akash-ophiurasoft <akash.nachan@ophiurasoft.com>
2023-02-08 12:59:10 +0530

    adding multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a = 2;
+    int b = 3;
+    int mul = a*b;
+
+    cout<<"Mutiply is:"<<mul<<endl;
+
+
+    return 0;
+}
